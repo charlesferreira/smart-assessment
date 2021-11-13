036e1de6044adf0359296d67aee75f85821fb2e2
Charles Ferreira <charles.dev@live.com>
2021-11-13 11:52:32 -0300

    add progress bar

--- projects/smart-clone/src/app/angular-material.module.ts
+++ projects/smart-clone/src/app/angular-material.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  exports: [LayoutModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule],
+  exports: [LayoutModule, MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatProgressBarModule],
 })
 export class AngularMaterialModule {}
