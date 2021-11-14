32ee94a4e89068d6d40191b077d8aa0e138deb2a
Charles Ferreira <charles.dev@live.com>
2021-11-13 22:04:07 -0300

    add hard-coded data to environments file

--- projects/smart-clone/src/environments/environment.ts
+++ projects/smart-clone/src/environments/environment.ts
@@ -2,9 +2,14 @@ export const environment = {
   production: false,
   mapbox: {
     accessToken: 'pk.eyJ1IjoiY2hhcmxlc2ZlcnJlaXJhIiwiYSI6ImNrdnV4M2xlczB6dGoyd28yNWRxb3BvZmUifQ.qIjzCMH4DjhVJml84G_Ehg',
-    // style: 'https://api.maptiler.com/maps/74a1fbc2-29a2-41f6-932e-48794340ff54/style.json?key=LmQtBBRUitvZZP7HybC7',
+    style: 'https://api.maptiler.com/maps/74a1fbc2-29a2-41f6-932e-48794340ff54/style.json?key=LmQtBBRUitvZZP7HybC7',
   },
   smart: {
     apiUrl: 'https://app.smartapartmentdata.com/List/json',
+    hardCoded: {
+      // the following data is hard-coded for the demo
+      listID: 5363950,
+      token: '5AE7DFB40500DDC03BC84BD3F0A8AC0F18784B1E',
+    },
   },
 };
